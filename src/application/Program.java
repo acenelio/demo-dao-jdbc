e3d4159a3232ca2399b4126a55d8fbd93ccc3097
Nelio Alves <acenelio@gmail.com>
2018-12-26 16:29:34 -0200

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 	}
 }
