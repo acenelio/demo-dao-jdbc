11cddc7b3cf89bd0e89287c692239a71e1189f6f
acenelio <acenelio@gmail.com>
2019-08-19 17:19:41 -0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -15,16 +15,16 @@ CREATE TABLE seller (
   FOREIGN KEY (DepartmentId) REFERENCES department (id)
 );
 
-INSERT INTO department VALUES 
-  (1,'Computers'),
-  (2,'Electronics'),
-  (3,'Fashion'),
-  (4,'Books');
+INSERT INTO department (Name) VALUES 
+  ('Computers'),
+  ('Electronics'),
+  ('Fashion'),
+  ('Books');
 
-INSERT INTO seller VALUES 
-  (1,'Bob Brown','bob@gmail.com','1998-04-21 00:00:00',1000,1),
-  (2,'Maria Green','maria@gmail.com','1979-12-31 00:00:00',3500,2),
-  (3,'Alex Grey','alex@gmail.com','1988-01-15 00:00:00',2200,1),
-  (4,'Martha Red','martha@gmail.com','1993-11-30 00:00:00',3000,4),
-  (5,'Donald Blue','donald@gmail.com','2000-01-09 00:00:00',4000,3),
-  (6,'Alex Pink','bob@gmail.com','1997-03-04 00:00:00',3000,2);
+INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId) VALUES 
+  ('Bob Brown','bob@gmail.com','1998-04-21 00:00:00',1000,1),
+  ('Maria Green','maria@gmail.com','1979-12-31 00:00:00',3500,2),
+  ('Alex Grey','alex@gmail.com','1988-01-15 00:00:00',2200,1),
+  ('Martha Red','martha@gmail.com','1993-11-30 00:00:00',3000,4),
+  ('Donald Blue','donald@gmail.com','2000-01-09 00:00:00',4000,3),
+  ('Alex Pink','bob@gmail.com','1997-03-04 00:00:00',3000,2);
