f3f4edfae6a9961ad15cd788f075d63fd38d22ec
acenelio <acenelio@gmail.com>
2020-04-19 18:55:26 -0300

    SQL datetime

--- database.sql
+++ database.sql
@@ -8,7 +8,7 @@ CREATE TABLE seller (
   Id int(11) NOT NULL AUTO_INCREMENT,
   Name varchar(60) NOT NULL,
   Email varchar(100) NOT NULL,
-  BirthDate datetime(6) NOT NULL,
+  BirthDate datetime NOT NULL,
   BaseSalary double NOT NULL,
   DepartmentId int(11) NOT NULL,
   PRIMARY KEY (Id),
